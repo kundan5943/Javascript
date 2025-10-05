05318ba0cc198695e3e68aebd21f7117e2e02fb3
Kundan Chaudhari <kundanchau624@gmail.com>
2025-10-05 08:00:21 +0530

    new files

--- 27_jfbjh.js
+++ 27_jfbjh.js
@@ -1,19 +1,5 @@
 //feature 1
 //javascript feature
-<<<<<<< HEAD
-let arr=[1,5,9,8,10,23,9];
-
-function printarr()
-{
-    for(el of arr)
-{
-    console.log(el);
-}
-
-}
-
-printarr();
-=======
 let arr=[2,8,9,42,10,23,9];
 let maxval;
 
@@ -22,4 +8,3 @@ for(el of arr)
     console.log(el);
     
 }
->>>>>>> branch2
